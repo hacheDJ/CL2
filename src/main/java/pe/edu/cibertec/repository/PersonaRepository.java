5a76655301d29d327a8628e5cf998a5e5b6b66b2
hacheDJ <this.dejotahachece@gmail.com>
2023-07-19 23:21:52 -0500

    pregunta 02

---
+++ src/main/java/pe/edu/cibertec/repository/PersonaRepository.java
@@ -0,0 +1,9 @@
+package pe.edu.cibertec.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import pe.edu.cibertec.model.Persona;
+
+public interface PersonaRepository extends CrudRepository<Persona, Integer>{
+
+}
